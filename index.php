e0798b55a71e8a02e6e3cda6cef4da6caf57e969
Paolo Christian Gallardo <Paolo Christian Gallardo>
2016-09-02 01:12:31 +0800

    EDIT:  changed the title | added meta description content

--- index.php
+++ index.php
@@ -3,12 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Starter</title>
-        <meta name="description" content="">
+        <title>Uranos Landing Page</title>
+        <meta name="description" content="Uranos Landing Page HTML5 Template. Fully reponsive. Modern. Slick. Crisp Design.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
 
         <link rel="stylesheet" type="text/css" href="dist/style.css">
     </head>
