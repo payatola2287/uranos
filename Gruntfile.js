789c7979bb02bcbca8433757d4aec87095c62210
Paolo Christian Gallardo <Paolo Christian Gallardo>
2016-09-02 01:03:56 +0800

    ADD: starting the project

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            A: {
+                files: ['src/css/*.css','src/*.css'],
+                tasks: ['default']
+            }
+        },
+        postcss: {
+            options: {
+                map: {
+                    inline: false, // save all sourcemaps as separate files...
+                    annotation: 'maps/css' // ...to the specified directory
+                },
+                processors: [
+                    require('precss')({ /* options */ }),
+                    require('postcss-color-function'),
+                    require("postcss-calc"),
+                    require("css-mqpacker"),
+                    require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                ]
+            },
+            dist: {
+                src: 'src/main.css',
+                dest: 'dist/style.css'
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist',
+                    src: ['style.css'],
+                    dest: 'dist',
+                    ext: '.min.css'
+                }]
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-webpack');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("css-mqpacker");
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['postcss:dist','cssmin']);
+
+};
---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Starter</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" type="text/css" href="dist/style.css">
+    </head>
+    <body>
+        <script src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js"></script>
+        <script src="dist/bundle.js"></script>
+        <script>
+            <!-- JS on the page -->
+        </script>
+    </body>
+</html>
