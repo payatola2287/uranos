f2e2cca6489b8cca01a95883fa31c43987fb9f55
Paolo Christian Gallardo <Paolo Christian Gallardo>
2016-09-03 09:58:50 +0800

    EDIT: added the html structure of the template

--- index.php
+++ index.php
@@ -10,6 +10,238 @@
         <link rel="stylesheet" type="text/css" href="dist/style.css">
     </head>
     <body>
+        <header id="site-header" class="site-header main-header">
+            <nav class="navbar navbar-default navbar-fixed-top navbar-theme">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+                            <span class="sr-only">Main Menu</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Brand</a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="main-menu">
+                        <ul class="nav navbar-nav navbar-right nav-theme">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Our Team</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        </header>
+        <section class="banner page-row uranos" id="banner">
+            <div class="hero-registration">
+                <p class="content-text">Discover the World of Awesome <strong>free PSD templates</strong></p>
+                <form class="form-horizontal basic-form ghost-form banner-reg-form">
+                    <div class="form-control-wrapper">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control ghost-input" id="email">
+                    </div>
+                    <div class="form-control-wrapper">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control ghost-input" id="password">
+                    </div>
+                    <button class="btn theme-btn">Registers</button>
+                </form>
+            </div>
+        </section>
+        <section class="page-row" id="services">
+            <div class="container theme-container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="service">
+                            <h2><i class="fa"></i>Print Services</h2>
+                            <p>Fusce eu tortor tristique, varius sem sollicitudin, viverra ante. Curabitur id metus velit. Suspendisse in justo nulla. </p>
+                        </div>
+                        <div class="service">
+                            <h2><i class="fa"></i>IT Services</h2>
+                            <p>Fusce eu tortor tristique, varius sem sollicitudin, viverra ante. Curabitur id metus velit. Suspendisse in justo nulla. </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-md-push-4">
+                        <div class="service">
+                            <h2><i class="fa"></i>Mobile Devices</h2>
+                            <p>Fusce eu tortor tristique, varius sem sollicitudin, viverra ante. Curabitur id metus velit. Suspendisse in justo nulla. </p>
+                        </div>
+                        <div class="service">
+                            <h2><i class="fa"></i>Print Services</h2>
+                            <p>Fusce eu tortor tristique, varius sem sollicitudin, viverra ante. Curabitur id metus velit. Suspendisse in justo nulla. </p>
+                        </div>
+                    </div>
+                    <figure class="col-md-4 col-md-pull-4 col-xs-6 iphone" id="iphone">
+                        <img src="img/iphone-768.png" srcset="img/iphone.png 900w" alt="IPHONE" title="IPHONE" class="img-iphone"/>
+                    </figure>
+                </div>
+            </div>
+        </section>
+        <section class="page-row" id="team">
+            <h1 class="page-row-title text-center">Meet our team<small>We make this APP!</small></h1>
+            <div class="container theme-container">
+                <div class="row">
+                    <div class="col-md-3 team-member">
+                        <img src="img/john.png" alt="John Doe" class="member-img">
+                        <h2 class="content-title name">John Doe</h2>
+                        <p class="content-text bio">Cras vel eleifend nisl, ac efficitur purus. Vestibulum vestibulum aliquam lectusmattis ac.</p>
+                    </div>
+                    <div class="col-md-3 team-member">
+                        <img src="img/joe.png" alt="Joe Doe" class="member-img">
+                        <h2 class="content-title name">Joe Doe</h2>
+                        <p class="content-text bio">Cras vel eleifend nisl, ac efficitur purus. Vestibulum vestibulum aliquam lectusmattis ac.</p>
+                    </div>
+                    <div class="col-md-3 team-member">
+                        <img src="img/Jane.png" alt="Jane Doe" class="member-img">
+                        <h2 class="content-title name">Jane Doe</h2>
+                        <p class="content-text bio">Cras vel eleifend nisl, ac efficitur purus. Vestibulum vestibulum aliquam lectusmattis ac.</p>
+                    </div>
+                    <div class="col-md-3 team-member">
+                        <img src="img/Jenny.png" alt="Jenny Doe" class="member-img">
+                        <h2 class="content-title name">Jenny Doe</h2>
+                        <p class="content-text bio">Cras vel eleifend nisl, ac efficitur purus. Vestibulum vestibulum aliquam lectusmattis ac.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="page-row" id="promo">
+            <div class="wrapper theme-wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p class="content-heading content-text">
+                                This is an awesome APP
+                            </p>
+                            <p class="content-text">
+                                Nam accumsan nunc sit amet elementum sollicitudin. Integer vel lacus eget tortor lobortis tincidunt sed eu dolor. Phasellus cursus augue ac pulvinar cursus.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p class="content-heading content-text">
+                            This is an awesome APP
+                        </p>
+                        <p class="content-text">
+                            Nam accumsan nunc sit amet elementum sollicitudin. Integer vel lacus eget tortor lobortis tincidunt sed eu dolor. Phasellus cursus augue ac pulvinar cursus.
+                        </p>
+                    </div>
+                    <figure class="mac-wrapper">
+                        <img src="img/mac-half.png" srcset="img/mac.png 900w" class="mac" alt="Widescreen View" />
+                    </figure>
+                </div>
+            </div>
+        </section>
+        <section class="page-row text-center" id="how-it-works">
+            <h1 class="row-header">
+                How our app works?
+                <small>We made this APP!</small>
+            </h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="icon-box icon-box-top">
+                            <i class="fa"></i>
+                            <p class="content-heading">
+                                Lorem Ipsum
+                            </p>
+                            <p class="content-text">
+                                Cras vel eleifend nisl, ac efficitur purus. Vestibulum vestibulum aliquam lectusmattis ac.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="icon-box icon-box-top">
+                            <i class="fa"></i>
+                            <p class="content-heading">
+                                Lorem Ipsum
+                            </p>
+                            <p class="content-text">
+                                Cras vel eleifend nisl, ac efficitur purus. Vestibulum vestibulum aliquam lectusmattis ac.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="icon-box icon-box-top">
+                            <i class="fa"></i>
+                            <p class="content-heading">
+                                Lorem Ipsum
+                            </p>
+                            <p class="content-text">
+                                Cras vel eleifend nisl, ac efficitur purus. Vestibulum vestibulum aliquam lectusmattis ac.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="icon-box icon-box-top">
+                            <i class="fa"></i>
+                            <p class="content-heading">
+                                Lorem Ipsum
+                            </p>
+                            <p class="content-text">
+                                Cras vel eleifend nisl, ac efficitur purus. Vestibulum vestibulum aliquam lectusmattis ac.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="btn btn-lg btn-theme" title="Download APP">Download Now</a>
+            </div>
+        </section>
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-2">
+                        <h3 class="content-heading">
+                            Proudly created by
+                        </h3>
+                        <p class="content-text">
+                            <span class="brand-name name-text">
+                                Lorem Ipsum
+                            </span>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-sm-2">
+                        <h3 class="content-heading">
+                            About Us
+                        </h3>
+                        <p class="content-text">
+                            Ad litora torquent Per conubia Nostra Per inceptos Himenaeos In consectetur Nisi sed blandit Tincidunt.
+                            <a href="#" title="Learn about the creators">Know about us</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-sm-2">
+                        <h3 class="content-heading">
+                            Other Apps
+                        </h3>
+                        <p class="content-text">
+                            Ad litora torquent Per conubia Nostra Per inceptos Himenaeos In consectetur Nisi sed blandit Tincidunt.
+                            <a href="#" title="Check the other apps">See all apps</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-sm-2">
+                        <h3 class="content-heading">
+                            Interested?
+                        </h3>
+                        <p class="content-text">
+                            Ad litora torquent Per conubia Nostra Per inceptos Himenaeos In consectetur Nisi sed blandit Tincidunt.
+                            <a href="#" title="Get started with my project">Contact Us</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <section class="copyright text-center">
+                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                Try awsome tool for desgners symu.co
+            </section>
+        </footer>
         <script src="https://code.jquery.com/jquery-{{JQUERY_VERSION}}.min.js"></script>
         <script src="dist/bundle.js"></script>
         <script>
